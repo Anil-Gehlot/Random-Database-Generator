ac3a334fd1c802643263ecd9e5c2c6d417dbd02c
harshyadav02 <hy043768@gmail.com>
2024-01-12 21:32:35 +0530

    This file contains all the views which are required for user interaction

--- MongoDB_views.py
+++ MongoDB_views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint , request , render_template
+import json
+from flask import Blueprint, jsonify , request , render_template ,send_file
 from MongoDB_functions import create_collection   # function to create collection in database
 from MongoDB_functions import create_database     # function to create database in database
 from MongoDB_functions import data_split          # function to split data into key and data type     
 from pymongo.errors import  PyMongoError , DuplicateKeyError
 from config import client 
+from MongoDB_functions import generate_schema_mongo
 
+# blueprint 
 mongo_db = Blueprint('mongo_db',__name__)
 
 
@@ -12,7 +15,7 @@ mongo_db = Blueprint('mongo_db',__name__)
 def collection_creation():
     # retriving database information from html form
     database = request.form["dbName"]
-    
+    print("This is database " ,database)
     # retriev ing number of collections from html form
     number_of_collections = int(request.form['numTables'])
 
@@ -20,10 +23,11 @@ def collection_creation():
 
 
 
-@mongo_db.route('/submit_collection_details/<database>/<int:number_of_collections>/', methods=['POST'])
-def table_details(database, number_of_collections=1):
+@mongo_db.route('/submit_collection_details/<mongo_database>/<int:number_of_collections>/', methods=['POST'])
+def table_details(mongo_database, number_of_collections):
     # list to hold all the messages 
-    messages = [] 
+    messages = []
+    errors = [] 
     
     try:
 
@@ -32,7 +36,10 @@ def table_details(database, number_of_collections=1):
         key_details_list = request.form.getlist('columnDetails')
 
         # database creation
-        database = create_database(database)
+        database = create_database(mongo_database)
+        print("Table route database " ,database)
+        print("Table route mongo_database " ,mongo_database)
+
         messages.append("Database Created Successfully")  # messages[0]
         # list to hold the key details for each collection
         
@@ -44,7 +51,7 @@ def table_details(database, number_of_collections=1):
 
         # creating collections and inserting documnet to each collection 
         for coll in range(len(collection_list)):
-           
+            data_type_error = False
             # collection creation
             table = create_collection(collection_list[coll], database)
             messages.append(f"Collection {collection_list[coll]} Created Successfully") 
@@ -58,14 +65,14 @@ def table_details(database, number_of_collections=1):
                 for key in range(len(key_list)):
                    
                     entry = data_split(key_list[key], {})
+                    
                     # store the entry data in the dictionary for the specific collection
                     collection_data[collection_list[key]] = entry
                 
                 # insert data into the appropriate collection
                 table.insert_one(collection_data[collection_list[coll]])
-            messages.append(f"Document inserted Successfully")
-        
-        return render_template('Mongo/output.html', message = messages)
+
+        return render_template('Mongo/output.html', messages = messages , db = mongo_database)
     
     # Handling runtime errors 
     except DuplicateKeyError as keyerror :
@@ -77,7 +84,6 @@ def table_details(database, number_of_collections=1):
         return render_template('Mongo/output.html' ,message = messages , error = mongoError)
 
     except Exception as errors:
-        
         return render_template('Mongo/output.html' ,message = messages , error = errors)
 
 
@@ -85,6 +91,23 @@ def table_details(database, number_of_collections=1):
         # client.close() 
         pass  
 
+@mongo_db.route('/download_schema/<db_name>/' , methods=['GET', 'POST'])
+def download_schema(db_name):
+    try:
+        db = client[db_name]
 
-    
+        if request.method == 'POST':
+            schema_result = generate_schema_mongo(db)
 
+            # Save the schema to a file
+            file_path = f"{db_name}_schema.json"
+            with open(file_path, 'w') as file:
+                json.dump(schema_result,file ,indent=2, default=str)
+
+            # Provide the file for download
+            return send_file(file_path, as_attachment=True)
+
+        return "Send a POST request to download the schema."
+
+    except Exception as e:
+        return jsonify({"error": f"Error connecting to MongoDB: {e}"}), 500
